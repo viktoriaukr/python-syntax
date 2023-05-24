98e45cd6439db7718c9667ee984e920a4c79679f
viktoriaukr <viktoriiauhliai@gmail.com>
2023-05-24 17:17:44 -0500

    python syntax

---
+++ words.py
@@ -0,0 +1,7 @@
+def print_upper_words(list_of_words):
+    for word in list_of_words:
+       word = word.upper()
+       print(word)
+        
+
+print(print_upper_words(["hello", "hey", "goodbye", "yo", "yes"]))
